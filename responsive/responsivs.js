23024a67f343e39175ef44e71ce9147961aa4a1e
Nisha Singhal <111162548+nishaYO@users.noreply.github.com>
2023-02-21 20:21:58 +0530

    added responsiveness to about page

--- responsive/responsivs.js
+++ responsive/responsivs.js
@@ -1,12 +1,3 @@
-// ABOUT PAGE
 
-if (window.innerWidth <= 400) {
-  const container = document.querySelector("#about");
-  container.classList.remove("flex-center");
-}
 
-// BLOGS PAGE
-if (window.innerWidth <= 400) {
-    const blgbtn = document.querySelectorAll("blg-btn");
-    blgbtn.style.padding = '10px, 20px';
-  }
+
